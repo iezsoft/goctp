8a29e842fce700590717650f81ee3bf1f33ca771
scs <altersoft@126.com>
2020-10-27 10:07:22 +0800

    update ctp version reference

--- libgoctp.go
+++ libgoctp.go
@@ -7,9 +7,7 @@
 package goctp
 
 /*
-#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.3.15_20190220_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.3.15_20190220_api_tradeapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
-#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.3.15_20190220_api_tradeapi_se_linux64
-#cgo windows LDFLAGS: -fPIC -L${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows -Wl,-rpath,${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows ${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows/thostmduserapi.lib ${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows/thosttraderapi.lib -lthostmduserapi -lthosttraderapi
-#cgo windows CPPFLAGS: -fPIC -I${SRCDIR}/api/6.3.15_20190220_tradeapi64_se_windows -DISLIB -DWIN32 -DLIB_MD_API_EXPORT -DLIB_TRADER_API_EXPORT
+#cgo linux LDFLAGS: -fPIC -L${SRCDIR}/api/v6.3.19_T1_20200423_api_tradeapi_se_linux64 -Wl,-rpath,${SRCDIR}/api/v6.3.19_T1_20200423_api_tradeapi_se_linux64 -lthostmduserapi_se -lthosttraderapi_se -lstdc++
+#cgo linux CPPFLAGS: -fPIC -I${SRCDIR}/api/v6.3.19_T1_20200423_api_tradeapi_se_linux64
 */
 import "C"
